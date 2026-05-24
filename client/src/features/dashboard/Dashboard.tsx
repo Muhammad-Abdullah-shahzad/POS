86297648f8e1ba65b40f6b5db6aa14a166a0f383
Abdullah Bin Sikandar LGU <fa-2023-bsse-067@lgu.edu.pk>
2026-05-24 16:59:52 +0500

    working

--- client/src/features/dashboard/Dashboard.tsx
+++ client/src/features/dashboard/Dashboard.tsx
@@ -1341,7 +1341,7 @@ const Dashboard = () => {
                         </Flex>
                         <Flex style={{ borderBottom: `1px solid ${customColors.border}`, flex: 1 }}>
                           <Flex flex={5} align="center" style={{ borderRight: `1px solid ${customColors.border}`, padding: '0 6px' }}>
-                            <Text size="12px" c="red.7" fw={600}>Discount</Text>
+                            <Text size="12px" c="red.7" fw={600}>Flat Disc.</Text>
                           </Flex>
                           <Flex flex={7} align="center" justify="flex-end" style={{ padding: '0 4px', backgroundColor: '#fff3f3' }}>
                             <TextInput
