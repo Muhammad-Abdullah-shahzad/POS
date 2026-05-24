1d47ed3a592b663e67b75faeaa9476ed19fd53fb
Abdullah Bin Sikandar LGU <fa-2023-bsse-067@lgu.edu.pk>
2026-05-24 17:03:03 +0500

    working

--- client/src/features/dashboard/Dashboard.tsx
+++ client/src/features/dashboard/Dashboard.tsx
@@ -1343,7 +1343,7 @@ const Dashboard = () => {
                           <Flex flex={5} align="center" style={{ borderRight: `1px solid ${customColors.border}`, padding: '0 6px' }}>
                             <Text size="10px" c="red.7" fw={600} style={{ whiteSpace: 'nowrap' }}>Flat Discount</Text>
                           </Flex>
-                          <Flex flex={7} align="center" justify="flex-end" style={{ padding: '0 4px', backgroundColor: '#fff3f3' }}>
+                          <Flex flex={7} align="center" justify="flex-end" style={{ padding: '0 6px', backgroundColor: '#fff3f3' }}>
                             <TextInput
                               value={flatDiscount}
                               onChange={(e) => {
